75d7527485a714cd451a5a8b4441e622f8536770
nrshagor <noorerabbishagor@gmail.com>
2025-01-22 17:20:51 +0600

    remove unnecessary file

--- database/seed.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (username, email, password)
-VALUES 
-('admin', 'admin@example.com', '$2y$10$e.ImkqUslBGRp5MJSZT3quZB8xtfOrrXjXae0ZZ6rBB15Q4HAYV9O'); -- Password: admin123
-
-INSERT INTO events (name, description, date, location, capacity, created_by)
-VALUES 
-('Tech Conference', 'Annual tech meet', '2025-05-01 10:00:00', 'Conference Hall', 200, 1);
